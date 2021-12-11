b6ca076b53e8a632dd2d4c0de1446b6cf5112289
Mateusz Goik <mateuszgoik1@wp.pl>
2021-12-11 14:11:46 +0100

    test

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import { Router } from '@roxi/routify'
-  import { routes } from '../.routify/routes'
+  // import { Router } from '@roxi/routify'
+  // import { routes } from '../.routify/routes'
   import ReloadPrompt from './lib/ReloadPrompt.svelte'
+import Index from './pages/index.svelte';
 </script>
 
-<Router {routes} />
+<!-- <Router {routes} /> -->
+<Index />
 
 <ReloadPrompt />
 
